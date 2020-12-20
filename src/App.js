b8d37e8de357178e307074e723e9d987c24cbe51
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2020-12-20 15:34:28 -0600

    validated the routing and added to app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
+import CssBaseline from '@material-ui/core/CssBaseline'
 import {BrowserRouter} from 'react-router-dom';
 import Router from './Routing';
 import Panel from './Components/Panel';
 
 function App() {
   return (
-    <div className="App">
-    <Panel/>
-    </div>
+ 
+    <CssBaseline>
+      <BrowserRouter>
+        <Panel/>
+        <Router/>
+      </BrowserRouter>
+    </CssBaseline>
+  
   );
 }
 
--- src/Routing.js
+++ src/Routing.js
@@ -7,13 +7,17 @@ import Projects from './Components/Projects';
 
 
 const Router = () => {
+  return (
   <Switch>
     <Route path='/' component={Home}/>
     <Route path='/About' component={About} />
     <Route path='/Resume' component={Resume} />
     <Route path='/Projects' component={Projects} /> 
-    <Route path='/Projects/All' />
+    {/* <Route path='/Projects/All' />
     <Route path='/Projects/FrontEnd' />
-    <Route path='/Projects/BackEnd' />
+    <Route path='/Projects/BackEnd' /> */}
   </Switch>
+  )
 }
+
+export default Router
