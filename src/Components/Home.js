f5ce08659695805a7ec1158c4dc84704685ec321
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2020-12-20 16:17:16 -0600

    removed references to canvas for now

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,23 +1,12 @@
-import React from 'react';
-import Canvas from './Canvas'
-import './Components.css';
+import React, {useState} from 'react';
 
 const Homepage = () => {
 
-  const draw = (ctx, frameCount) => {
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
-    ctx.fillStyle = '#ff00de'
-    ctx.beginPath()
-    ctx.arc(50, 100, 20*Math.sin(frameCount*0.05)**2, 0, 2*Math.PI)
-    ctx.fill()
-  }
-
   return (
     <div className="Homepage">
-      <Canvas draw={draw}/>
       <h1>Natalie Kendrick</h1>
     </div>
-  );
+  )
 }
 
 export default Homepage;
