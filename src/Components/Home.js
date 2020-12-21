ac083aff6dd16bc406144d69e87772187b778e98
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2020-12-21 09:49:51 -0600

    resized and colored icons

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,16 +3,19 @@ import { FaLinkedin } from "@react-icons/all-files/fa/FaLinkedin";
 import { FaGoogle } from "@react-icons/all-files/fa/FaGoogle";
 import { FaTwitterSquare } from "@react-icons/all-files/fa/FaTwitterSquare";
 import { FaGithubAlt } from "@react-icons/all-files/fa/FaGithubAlt";
+import './Components.css'
 
 const Homepage = () => {
 
   return (
     <div className="Homepage">
       <h1>Natalie Kendrick</h1>
-      <FaLinkedin />
-      <FaGoogle />
-      <FaTwitterSquare/>
-      <FaGithubAlt/>
+   
+        <FaLinkedin className={'homeIcons'} />
+        <FaGoogle className={'homeIcons'}/>
+        <FaTwitterSquare className={'homeIcons'}/>
+        <FaGithubAlt className={'homeIcons'}/> 
+    
     </div>
   )
 }
