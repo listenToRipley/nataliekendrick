94789894e99df6943567b1765f8235b5cdde1cd5
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2020-12-21 11:22:56 -0600

    pdf html doc of resume kept for reference

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -3,6 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
+import { FaGoogle } from "@react-icons/all-files/fa/FaGoogle";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -12,7 +13,12 @@ const useStyles = makeStyles((theme) => ({
   paper: {
     padding: theme.spacing(2),
     margin: 'auto',
-    maxWidth: 500,
+    maxWidth: 700,
+  },
+  line: {
+    display: 'block',
+    color: 'grey',
+    backgroundColor: 'grey'
   },
 }));
 
@@ -31,9 +37,23 @@ const Resume = () => {
         alignItems="center"
         spacing={2}>
           <Grid
-          item xs> 
-          Natalie Kendrick
+          item xs={3}> 
+            <Typography>Natalie Kendrick</Typography>
+            <p>Full Stack Developer</p>
           </Grid>
+
+          <Grid
+          item xs={8}> 
+            <Typography>About</Typography>
+            <Paper className={classes.line}>.</Paper>
+            <p>Hard-working professional with over 10 years of work experience in a variety of fields and a working knowledge of application development, database design and the importance of leveraging those skills in creative and innovative approaches to problems while synthesizing the business and client needs.</p>
+          </Grid>
+
+          <Grid
+          item xs={3}> 
+            <FaGoogle/> 
+          </Grid>
+
         </Grid>
     </Paper>
     </div>
