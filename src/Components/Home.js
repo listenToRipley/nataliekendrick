988ae417ed9fb18dc186e8794f79186fb469cf0c
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2020-12-21 09:38:56 -0600

    added icons

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { FaLinkedin } from "@react-icons/all-files/fa/FaLinkedin";
+import { FaGoogle } from "@react-icons/all-files/fa/FaGoogle";
+import { FaTwitterSquare } from "@react-icons/all-files/fa/FaTwitterSquare";
+import { FaGithubAlt } from "@react-icons/all-files/fa/FaGithubAlt";
 
 const Homepage = () => {
 
   return (
     <div className="Homepage">
       <h1>Natalie Kendrick</h1>
+      <FaLinkedin />
+      <FaGoogle />
+      <FaTwitterSquare/>
+      <FaGithubAlt/>
     </div>
   )
 }
--- src/Components/Panel.js
+++ src/Components/Panel.js
@@ -13,7 +13,7 @@ const useStyles = makeStyles((theme) => ({
 
   }, 
   name: {
-    justifyItem: 'space-evenly'
+    justifyItem: 'center'
   }
 }));
 
@@ -29,23 +29,27 @@ const Panel = () =>  {
   return (
     <div className={classes.root}>
       <AppBar position="static">
-        <Typography className={classes.name}>Natalie Kendrick</Typography>
+        {/* <Typography className={classes.name}>Welcome</Typography> */}
         <Tabs value={value} onChange={handleChange} aria-label="menu items" centered={true}>
           <Tab label="Home" 
                 component={Link}
                 to='/home' 
+                aria-label='home'
           />
           <Tab label="About"
                 component={Link}
                 to='/about'  
+                aria-label='about'
           />
           <Tab label="Resume"  
             component={Link}
             to='/resume' 
+            aria-label='resume'
           />
           <Tab label="Projects" 
               component={Link}
               to='/projects' 
+              aria-label='projects'
           />
         </Tabs>
       </AppBar>
