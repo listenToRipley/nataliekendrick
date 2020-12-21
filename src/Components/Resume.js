84fccd60df5e4d42a646b4b2aa877d681c435db8
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2020-12-21 10:15:05 -0600

    decided on grid for resume page

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -1,10 +1,42 @@
-import React from 'react'
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    margin: '2%'
+  },
+  paper: {
+    padding: theme.spacing(2),
+    margin: 'auto',
+    maxWidth: 500,
+  },
+}));
+
+
 
 const Resume = () => {
+  const classes = useStyles();
+
   return (
-    <h1>
-      Hello, my resume is here
-    </h1>
+    <div className={classes.root}>
+      <Paper className={classes.paper}>
+        <Grid
+        container
+        direction="row"
+        jusitify="center"
+        alignItems="center"
+        spacing={2}>
+          <Grid
+          item xs> 
+          Natalie Kendrick
+          </Grid>
+        </Grid>
+    </Paper>
+    </div>
   )
 }
 
