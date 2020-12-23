8323b932b1a906b6c545e10b764e985e5c77fb0d
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2020-12-23 10:53:44 -0600

    image coming through

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -15,6 +15,7 @@ import FavoriteIcon from '@material-ui/icons/Favorite';
 import ShareIcon from '@material-ui/icons/Share';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+import clicknspin from './projectImgs/clicknspin.png'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -23,6 +24,7 @@ const useStyles = makeStyles((theme) => ({
   media: {
     height: 0,
     paddingTop: '56.25%', // 16:9
+    marginTop:'30',
   },
   expand: {
     transform: 'rotate(0deg)',
@@ -52,7 +54,7 @@ const Projects = () => {
       <CardHeader
         avatar={
           <Avatar aria-label="recipe" className={classes.avatar}>
-            R
+            C & S
           </Avatar>
         }
         action={
@@ -65,7 +67,7 @@ const Projects = () => {
       />
       <CardMedia
         className={classes.media}
-        image={require{'./projectImgs/clicknspin.png'}}
+        image={clicknspin}
         title="click and spin project"
       />
       <CardContent>
@@ -77,7 +79,7 @@ const Projects = () => {
         <IconButton aria-label="add to favorites">
           <FavoriteIcon />
         </IconButton>
-        <IconButton aria-label="share">
+        <IconButton aria-label="medias">
           <ShareIcon />
         </IconButton>
         <IconButton
@@ -93,28 +95,18 @@ const Projects = () => {
       </CardActions>
       <Collapse in={expanded} timeout="auto" unmountOnExit>
         <CardContent>
-          <Typography paragraph>Method:</Typography>
+          <Typography paragraph>Stuff</Typography>
           <Typography paragraph>
-            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
-            minutes.
+            =
           </Typography>
           <Typography paragraph>
-            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
-            heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
-            browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving chicken
-            and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion, salt and
-            pepper, and cook, stirring often until thickened and fragrant, about 10 minutes. Add
-            saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
+          ?
           </Typography>
           <Typography paragraph>
-            Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook
-            without stirring, until most of the liquid is absorbed, 15 to 18 minutes. Reduce heat to
-            medium-low, add reserved shrimp and mussels, tucking them down into the rice, and cook
-            again without stirring, until mussels have opened and rice is just tender, 5 to 7
-            minutes more. (Discard any mussels that don’t open.)
+            1
           </Typography>
           <Typography>
-            Set aside off of the heat to let rest for 10 minutes, and then serve.
+          1
           </Typography>
         </CardContent>
       </Collapse>
