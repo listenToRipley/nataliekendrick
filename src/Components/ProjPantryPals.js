468a10afe6f02dacf8b177e58c1ea14377fbbd30
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2020-12-24 14:13:38 -0600

    outlined the pantry cards

---
+++ src/Components/ProjPantryPals.js
@@ -0,0 +1,8 @@
+//https://github.com/listenToRipley/ppApp/blob/master/eer.png
+
+//https://github.com/listenToRipley/capstone
+
+//https://pantrypals.herokuapp.com/ 
+
+//include a reel for the project process, need to take pictures of it 
+//card will have to have a link to it. 
