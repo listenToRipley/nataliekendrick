c9a41edd6ecc73dfb5471c9ae85d7bfd4b2d6f6e
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2020-12-26 09:50:37 -0600

    project list added

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -6,11 +6,13 @@ import Mustacchio from './ProjMustacchio'
 import Animate from './ProjAnimate'
 import FirstToDo from './ProjFirstToDo'
 import TicTacToe from './ProjTicTacToe'
+import AddressBook from './ProjAddressBook'
 
 const Projects = () => {
 
   return (
    <div>
+    <AddressBook/>
     <SilverSea/>
     <ClicknSpin/>
     <TicTacToe/>
