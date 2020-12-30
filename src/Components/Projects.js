647bee3cd3f1750cba2c4974f6bfed3cbe4dcc4c
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2020-12-30 11:46:32 -0600

    project css updated

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -15,17 +15,19 @@ const Projects = () => {
 
   return (
    <div>
-    <PantryPals/>
-    <SmallBusiness/>
-    <AddressBook/>
-    <DodgeBall/>
-    <PigLatin/>
-    <SilverSea/>
-    <ClicknSpin/>
-    <TicTacToe/>
-    <NASACard/>
-    <Mustacchio/>
-    <Animate/>
+    <div className="projectBody">
+      <PantryPals/>
+      <SmallBusiness/>
+      <AddressBook/>
+      <DodgeBall/>
+      <PigLatin/>
+      <SilverSea/>
+      <ClicknSpin/>
+      <TicTacToe/>
+      <NASACard/>
+      <Mustacchio/>
+      <Animate/>
+    </div>
     <p> Please feel free to review other backend projects here</p>
     <p> Or generate problems worked on through <a href="https://repl.it/@listenToRipley">repl.it</a> or with <a href="https://www.codewars.com/users/minonOfLight">code wars</a> </p>
    </div>
