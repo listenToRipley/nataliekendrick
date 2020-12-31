3ce30646878a70837b763d01cd6d3b76d76c4b3a
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2020-12-31 10:31:14 -0600

    update tic tac toe css

--- src/Components/ProjTicTacToe.js
+++ src/Components/ProjTicTacToe.js
@@ -8,6 +8,7 @@ import Typography from '@material-ui/core/Typography';
 import { FaHtml5 } from "@react-icons/all-files/fa/FaHtml5";
 import { FaCss3Alt } from "@react-icons/all-files/fa/FaCss3Alt";
 import { FaJs} from "@react-icons/all-files/fa/FaJs";
+import ButtonGroup from '@material-ui/core/ButtonGroup';
 import Button from '@material-ui/core/Button';
 import ticTacToe from './projectImgs/TicTacToe.png';
 import './Components.css';
@@ -30,22 +31,27 @@ return (
       <Typography paragraph>
           First attempt at tic tac toe
       </Typography>
-        <Typography variant="body2" color="textSecondary" component="p">
-          <FaHtml5/>
-          <FaCss3Alt/>
-          <FaJs/>
+        <Typography className="ticTacToeBadges">
+
+          <FaHtml5 className="badges"/>
+          <FaCss3Alt className="badges"/>
+          <FaJs className="badges"/>
+
         </Typography>
-          <Typography paragraph>
+          <Typography className="skills">
              HTML5, CSS, Javascript
           </Typography>
       <CardActions>
-        <Button onClick={() => window.open('https://listentoripley.github.io/ACA_web101_Natalie_Kendrick/src/tictactoe/ttt.html')}>
-        View Site
-        </Button>
- 
-        <Button onClick={() => window.open('https://github.com/listenToRipley/ACA_web101_Natalie_Kendrick/tree/master/src/tictactoe')}>
-        View Code
-        </Button>
+
+        <ButtonGroup fullWidth={true} variant="text" size="large" component="div">
+           <Button className="viewButton" onClick={() => window.open('https://listentoripley.github.io/ACA_web101_Natalie_Kendrick/src/tictactoe/ttt.html')}>
+           View Site
+           </Button>
+
+           <Button className="viewButton" onClick={() => window.open('https://github.com/listenToRipley/ACA_web101_Natalie_Kendrick/tree/master/src/tictactoe')}>
+           View Code
+           </Button>
+        </ButtonGroup>
   
         </CardActions>
       </CardContent>
