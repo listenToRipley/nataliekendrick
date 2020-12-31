f83f0873a164ec9fa482feccee1df3349a5502d3
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2020-12-31 10:52:01 -0600

    updated animate card

--- src/Components/ProjAnimate.js
+++ src/Components/ProjAnimate.js
@@ -5,8 +5,9 @@ import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
 import Typography from '@material-ui/core/Typography';
-import { FaHtml5 } from "@react-icons/all-files/fa/FaHtml5"
-import { FaCss3Alt } from "@react-icons/all-files/fa/FaCss3Alt"
+import { FaHtml5 } from "@react-icons/all-files/fa/FaHtml5";
+import { FaCss3Alt } from "@react-icons/all-files/fa/FaCss3Alt";
+import ButtonGroup from '@material-ui/core/ButtonGroup';
 import Button from '@material-ui/core/Button';
 import animate from './projectImgs/animate.png'
 import './Components.css'
@@ -26,24 +27,29 @@ return (
     title="animated boxes"
   />
   <CardContent>
-  <Typography paragraph>
+  <Typography>
     Working with animation with CSS, hover each square to see the transitions
   </Typography>
-    <Typography variant="body2" color="textSecondary" component="p">
-      <FaHtml5/>
-      <FaCss3Alt/>
+    <Typography className="animateBadges">
+
+      <FaHtml5 className="badges"/>
+      <FaCss3Alt className="badges"/>
+
     </Typography>
-      <Typography paragraph>
+      <Typography className="skills">
          HTML5, CSS
       </Typography>
   <CardActions>
-    <Button onClick={() => window.open('https://listentoripley.github.io/ACA_web101_Natalie_Kendrick/src/animate/animate.html')}>
-    View Site
-    </Button>
 
-    <Button onClick={()=> window.open('https://github.com/listenToRipley/ACA_web101_Natalie_Kendrick/tree/master/src/animate')}>
-    View Code
-    </Button>
+    <ButtonGroup fullWidth={true} variant="text" size="large" component="div">
+      <Button className="viewButton" onClick={() => window.open('https://listentoripley.github.io/ACA_web101_Natalie_Kendrick/src/animate/animate.html')}>
+      View Site
+      </Button>
+  
+      <Button className="viewButton" onClick={()=> window.open('https://github.com/listenToRipley/ACA_web101_Natalie_Kendrick/tree/master/src/animate')}>
+      View Code
+      </Button>
+    </ButtonGroup>
 
     </CardActions>
   </CardContent>
