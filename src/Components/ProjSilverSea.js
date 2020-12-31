72d472cfd00c67a0e06a01a1cbcf179267fd6552
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2020-12-31 10:00:49 -0600

    silve sea cards updated

--- src/Components/ProjSilverSea.js
+++ src/Components/ProjSilverSea.js
@@ -7,6 +7,7 @@ import CardActions from '@material-ui/core/CardActions';
 import Typography from '@material-ui/core/Typography';
 import { FaHtml5 } from "@react-icons/all-files/fa/FaHtml5";
 import { FaCss3Alt } from "@react-icons/all-files/fa/FaCss3Alt";
+import ButtonGroup from '@material-ui/core/ButtonGroup';
 import Button from '@material-ui/core/Button';
 import silverSea from './projectImgs/SilverSea.png';
 import './Components.css';
@@ -18,7 +19,7 @@ return (
     <Card className='cardBody'>
       <CardHeader
         title="Silver Sea Website"
-        subheader="January 2020"
+        subheader="February 2020"
       />
       <CardMedia
         className='cardMedia'
@@ -29,21 +30,28 @@ return (
       <Typography paragraph>
       Full website for Silver Sea Jewelry 
       </Typography>
-        <Typography variant="body2" color="textSecondary" component="p">
-          <FaHtml5/>
-          <FaCss3Alt/>
+        <Typography className="silverSeaBadges">
+
+          <FaHtml5 className="badges"/>
+          <FaCss3Alt className="badges"/>
+
         </Typography>
-          <Typography paragraph>
+
+          <Typography className="skills">
              HTML5, CSS
           </Typography>
       <CardActions>
-        <Button onClick={() => window.open('https://listentoripley.github.io/ACA_web101_Natalie_Kendrick/src/checkpoint2/checkpoint2.html')}>
-        View Site
-        </Button>
- 
-        <Button onClick={()=> window.open('https://github.com/listenToRipley/ACA_web101_Natalie_Kendrick/tree/master/src/checkpoint2')}>
-        View Code
-        </Button>
+
+        <ButtonGroup fullWidth={true} variant="text" size="large" component="div">
+          <Button className="viewButton" onClick={() => window.open('https://listentoripley.github.io/ACA_web101_Natalie_Kendrick/src/checkpoint2/checkpoint2.html')}>
+          View Site
+          </Button>
+  
+          <Button className="viewButton" onClick={()=> window.open('https://github.com/listenToRipley/ACA_web101_Natalie_Kendrick/tree/master/src/checkpoint2')}>
+          View Code
+          </Button>
+
+        </ButtonGroup>
   
         </CardActions>
       </CardContent>
