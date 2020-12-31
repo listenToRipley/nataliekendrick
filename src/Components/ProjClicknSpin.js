917cc456b07210a96953a62818e4ce3b65f37d1a
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2020-12-31 10:25:39 -0600

    the click and spin css updated

--- src/Components/ProjClicknSpin.js
+++ src/Components/ProjClicknSpin.js
@@ -8,6 +8,7 @@ import Typography from '@material-ui/core/Typography';
 import { FaHtml5 } from "@react-icons/all-files/fa/FaHtml5";
 import { FaCss3Alt } from "@react-icons/all-files/fa/FaCss3Alt";
 import { FaBootstrap } from "@react-icons/all-files/fa/FaBootstrap";
+import ButtonGroup from '@material-ui/core/ButtonGroup';
 import Button from '@material-ui/core/Button';
 import clicknspin from './projectImgs/clicknspin.png';
 import './Components.css'
@@ -19,7 +20,7 @@ return (
 <Card className='cardBody'>
       <CardHeader
         title="Click and Spin"
-        subheader="February 2020"
+        subheader="March 2020"
       />
       <CardMedia
         className='cardMedia'
@@ -30,22 +31,27 @@ return (
       <Typography paragraph>
           Spin counter
       </Typography>
-        <Typography variant="body2" color="textSecondary" component="p">
-          <FaHtml5/>
-          <FaCss3Alt/>
-          <FaBootstrap/>
+        <Typography className="clickBadges">
+
+          <FaHtml5 className="badges"/>
+          <FaCss3Alt className="badges"/>
+          <FaBootstrap className="badges"/>
+
         </Typography>
-          <Typography paragraph>
+          <Typography className="skills">
              HTML5, CSS, Bootstrap
           </Typography>
       <CardActions>
-        <Button onClick={() => window.open('https://listentoripley.github.io/ACA_web101_Natalie_Kendrick/src/checkpoint3/click/click.html')}>
-        View Site
-        </Button>
- 
-        <Button onClick={() => window.open('https://github.com/listenToRipley/ACA_web101_Natalie_Kendrick/tree/master/src/checkpoint3')}>
-        View Code
-        </Button>
+
+        <ButtonGroup fullWidth={true} variant="text" size="large" component="div">
+          <Button className="viewButton" onClick={() => window.open('https://listentoripley.github.io/ACA_web101_Natalie_Kendrick/src/checkpoint3/click/click.html')}>
+          View Site
+          </Button>
+  
+          <Button className="viewButton" onClick={() => window.open('https://github.com/listenToRipley/ACA_web101_Natalie_Kendrick/tree/master/src/checkpoint3')}>
+          View Code
+          </Button>
+        </ButtonGroup>
   
         </CardActions>
       </CardContent>
