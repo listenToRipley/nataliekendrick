addc13fdab9201f8f101dde86153481486601e20
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2021-01-07 21:21:13 -0600

    updated outlin for about css

--- src/Components/About.js
+++ src/Components/About.js
@@ -6,13 +6,16 @@ import './Components.css';
 const About = () => {
   return (
     <Paper className="aboutPage">
-    <Typography align={'center'} variant="h2" component="h3" gutterBottom>
+    <Typography className="aboutName" align='center' variant="h3" component="h4" gutterBottom>
       Natalie Kendrick
     </Typography> 
+    <Typography className="aboutTitle" variant="caption"  align='center' display="block">Time traveler</Typography>
+    <div className="aboutBody">
     <Typography paragraph>
-      Native desert rat of Tucson Arizona, current transplant to Austin Texas. 
-    </Typography>
-    <Typography paragraph> Natalie is passionate about coding, animals, cooking and movies.</Typography>
+       Native desert rat of Tucson Arizona, current transplant to  Austin Texas. 
+     </Typography>
+     <Typography paragraph> Natalie is passionate about coding,  animals, cooking and movies.</Typography>
+    </div>
   </Paper>
   )
 }
