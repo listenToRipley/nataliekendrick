6e5df4ef0f1ad3ae8c477a57f972b507f753278e
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2021-01-10 21:34:22 -0600

    added profile images

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import Avatar from '@material-ui/core/Avatar'
 import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography'
+import Typography from '@material-ui/core/Typography';
+import profile from './projectImgs/profile.jpeg';
 import './Components.css';
 
 const About = () => {
   return (
     <Paper className="aboutPage">
-    <Typography className="aboutName" align='center' variant="h3" component="h4" gutterBottom>
-      Natalie Kendrick
-    </Typography> 
+    <Avatar
+      className="profilePic"
+      alter='Natalie Kendrick' 
+      sizes={50}
+      src={profile}
+      >
+     </Avatar>
+     <Typography className="aboutName"   align='center' variant="h3"   component="h4" gutterBottom>
+       Natalie Kendrick
+     </Typography> 
     <Typography className="aboutTitle" variant="caption"  align='center' display="block">Time traveler</Typography>
     <div className="aboutBody">
     <Typography paragraph>
