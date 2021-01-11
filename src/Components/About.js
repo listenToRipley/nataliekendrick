d697471bbf11947d6253b862f11e7f3d55962e80
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2021-01-11 17:00:10 -0600

    body and quote look good

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import CardMedia from '@material-ui/core/CardMedia';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import profile from './projectImgs/profile.jpeg';
@@ -8,16 +7,16 @@ import './Components.css';
 const About = () => {
   return (
     <Paper className="aboutPage">
-     <Typography className="aboutName"   align='center' variant="h3"   component="h4" gutterBottom>
-     <img
+         <img
       className="profilePic"
       alter='Natalie Kendrick' 
       src={profile}
       >
      </img> 
+     <p className="aboutName" gutterBottom>
        Natalie Kendrick
-     </Typography> 
-    <Typography className="aboutTitle" variant="caption"  align='center' display="block">“The most dangerous phrase in the language is we've always done” ~ Grace Hopper</Typography>
+     </p> 
+    <p className="aboutQuote">   “The most dangerous phrase in the language is we've always done” ~ Grace Hopper</p>
     <div className="aboutBody">
     <Typography paragraph>
        Native desert rat of Tucson Arizona, current transplant to  Austin Texas. 
