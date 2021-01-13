bb12636bc03994cd40a33191c58b087d21096bb6
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2021-01-12 19:36:10 -0600

    basic carousel working for planning

--- src/Components/PantryPalsPlanning.js
+++ src/Components/PantryPalsPlanning.js
@@ -1,40 +1,45 @@
 import React from 'react';
 import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography'
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from 'react-responsive-carousel';
+import Typography from '@material-ui/core/Typography';
+import Carousel from 'react-material-ui-carousel';
 //pictures
-import eer1 from './projectImgs/pantryPalPlanning/eer1.png'
+import actionRel1 from './projectImgs/pantryPalPlanning/actionRel1.jpg';
+import actionRel2 from './projectImgs/pantryPalPlanning/actionRel2.jpg';
+import actionRel3 from './projectImgs/pantryPalPlanning/actionRel3.jpg';
+import actionRel4 from './projectImgs/pantryPalPlanning/actionRel4.jpg';
+import actionRel5 from './projectImgs/pantryPalPlanning/actionRel5.jpg';
+import actionVerbs from './projectImgs/pantryPalPlanning/actionVerbs.jpg';
+import eer1 from './projectImgs/pantryPalPlanning/eer1.png';
+import eer2 from './projectImgs/pantryPalPlanning/eer2.png';
 
 import './Components.css';
 
 
 const PantryPalPlanning = () => {
 
+  let img = [ 
+    {
+      name: 'EER 1',
+      description: 'first sql relationship diagram',
+      image: eer1
+    },
+    {
+      name: 'EER 2',
+      description: 'second sql relationship diagram',
+      image: eer2
+    }
+  ]
+
   return (
     <div className='planning'>
       <Paper className='planningPaper'>
-      <Typography className='planningTitle'>Pantry Pals Planning</Typography>
+      <div className='planningTitle'>
+      <Typography >Pantry Pals Planning</Typography>
+      </div>
        <Carousel className='carousel' showArrows={true} >
-          <div>
-            <img src="./projectImgs/pantryPalPlanning/eer1.png" />
-            <p className="legend">Original EER</p>
-          </div>
-
-          <div>
-            <img src="./projectImgs/pantryPalPlanning/eer2.png" />
-            <p className="legend">Second EER</p>
-          </div>
-
-          <div>
-            <img src="./projectImgs/pantryPalPlanning/eer3.png" />
-            <p className="legend">Third EER</p>
-          </div>
-
-          <div>
-            <img src="./projectImgs/pantryPalPlanning/eerFinal.png" />
-            <p className="legend">Final EER</p>
-          </div>
+       {
+                img.map( (item, i) => <img className='planImg' key={i} des={item.description} src={item.image} /> )
+            }
         </Carousel>
         <a href="https://docs.google.com/presentation/d/19xS8029_NRw05XWOCFp0lhlehDhmF07N_eE1Eif6Lfg/edit?usp=sharing">Original Concept</a>
       </Paper>
