98137b92446e29b2eff1b41183f81b564188d91c
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2021-01-17 15:44:43 -0600

    removed pig latin from projects list

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Typography from '@material-ui/core/Typography'
 import AddressBook from './ProjAddressBook'
 import DodgeBall from './ProjDodgeBall';
-import PigLatin from './ProjPigLatin'
 import SmallBusiness from './ProjSmallBusiness';
 import PantryPals from './ProjPantryPals'
 import CodeSample from './ProjCodeSamples';
@@ -18,7 +17,6 @@ const Projects = () => {
       <SmallBusiness/>
       <AddressBook/>
       <DodgeBall/>
-      <PigLatin/>
       <AllProjects/>
     </div>
     <div className="projectAdds">
