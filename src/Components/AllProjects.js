251a4607ef0ab2dade29985dd1e6f7ebe2cd5bb5
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2021-01-17 15:21:18 -0600

    update with tic tac toe

--- src/Components/AllProjects.js
+++ src/Components/AllProjects.js
@@ -5,20 +5,36 @@ import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
 import Typography from '@material-ui/core/Typography';
-import { FaHtml5 } from "@react-icons/all-files/fa/FaHtml5";
-import { FaCss3Alt } from "@react-icons/all-files/fa/FaCss3Alt";
 import ButtonGroup from '@material-ui/core/ButtonGroup';
 import Button from '@material-ui/core/Button';
+//badges
+import { FaHtml5 } from "@react-icons/all-files/fa/FaHtml5";
+import { FaCss3Alt } from "@react-icons/all-files/fa/FaCss3Alt";
+import { FaJs} from "@react-icons/all-files/fa/FaJs";
 //images
 import animate from './projectImgs/animate.png';
 import mustacchio from './projectImgs/mustacchio.png';
 import nasa from './projectImgs/NASA.png';
+import ticTacToe from './projectImgs/TicTacToe.png';
 
 import './Components.css';
 
 const AllProjects = () => {
 
   let projects = [
+    {
+      num: 4,  
+      header: 'Tic Tac Toe',
+      completed: 'February 2020',
+      image: ticTacToe,
+      imageTitle: 'First Tic Tac Toe',
+      description: 'First attempt at tic tac toe',
+      badges: [<FaHtml5/>, <FaCss3Alt/>, <FaJs/>],
+      color: '#818181;',
+      skills: 'HTML5, CSS, Javascript',
+      site: 'https://listentoripley.github.io/ACA_web101_Natalie_Kendrick/src/tictactoe/ttt.html',
+      code: 'https://github.com/listenToRipley/ACA_web101_Natalie_Kendrick/tree/master/src/tictactoe'
+    },
     {
       num: 3,  
       header: 'NASA Contact Page',
