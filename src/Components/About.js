f9ae50fb7adbc2dd228df8a923e2bd9586dfb597
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2021-01-19 20:29:43 -0600

    updated text on about page

--- src/Components/About.js
+++ src/Components/About.js
@@ -24,10 +24,11 @@ const About = () => {
     </em> ~ Grace Hopper</p>
     <div className="aboutBody">
     <Typography paragraph>
-    A motivated professional with extensive experiences ranging from web development to customer service. I believe in continuous learner, big picture thinking and keeping in mind business needs. I am flexible, organized and determined.
+    I am motivated professional with extensive experiences ranging from web development to fine arts to customer service. Due to this, I bring a unique protective to the table. I believe in continuous learner, big picture thinking and while keeping in mind business needs. I am flexible, organized and determined.
     </Typography>
+    <Typography> I am also goal-orientated and creatively drive. I am passionate about making sure everyone get what they need from the project. This makes me a self starter who thrives in when it comes to problem solving. I am also keep an eye on my team to make sure they have what they need to succeed as well. I believe that my team success is my own.</Typography>
     <Typography paragraph>
-    I love hiking, gaming, cooking and hanging out with my husband and dog.
+    In my spare time I love to read, hike with my dog, doing yoga, and play board games. I love, love, love food and cooking. 
     </Typography>
     </div>
   </Paper>
