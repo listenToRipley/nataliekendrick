1873b3344c807eb315eaf4a8fb4443eb6e771b00
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2021-01-19 20:35:53 -0600

    change about language a little more

--- src/Components/About.js
+++ src/Components/About.js
@@ -24,11 +24,14 @@ const About = () => {
     </em> ~ Grace Hopper</p>
     <div className="aboutBody">
     <Typography paragraph>
-    I am motivated professional with extensive experiences ranging from web development to fine arts to customer service. Due to this, I bring a unique protective to the table. I believe in continuous learner, big picture thinking and while keeping in mind business needs. I am flexible, organized and determined.
+    I am motivated professional with extensive experiences ranging from web development to fine arts to customer service. Due to this, I bring a unique protective to the table. I believe in continuous learner and big picture thinking. I am flexible, organized and determined.
     </Typography>
-    <Typography> I am also goal-orientated and creatively drive. I am passionate about making sure everyone get what they need from the project. This makes me a self starter who thrives in when it comes to problem solving. I am also keep an eye on my team to make sure they have what they need to succeed as well. I believe that my team success is my own.</Typography>
+    <Typography> I am also goal-orientated and creatively drive. I am passionate about making sure everyone get what they need from the project. I keep an eye on my team to make sure they have what they need to succeed as well. I believe that my team success is my own. All of this make me a proactive, self-starter who thrives in when it comes to problem solving.</Typography>
     <Typography paragraph>
-    In my spare time I love to read, hike with my dog, doing yoga, and play board games. I love, love, love food and cooking. 
+    In my spare time I love to read, hike with my dog, doing yoga, and play board games. I love, love, love food and cooking. I am also a massive movie buff and my international television. 
+    </Typography>
+    <Typography>
+      I look forward to hearing from you and have a fantastic adventure today. 
     </Typography>
     </div>
   </Paper>
