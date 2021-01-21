24fee4f950db167b194017403fcec0f0777a99ea
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2021-01-21 17:52:39 -0600

    better and worse

--- src/Components/About.js
+++ src/Components/About.js
@@ -9,25 +9,28 @@ import './Components.css';
 const About = () => {
   return (
     <Paper className="aboutPage">
-    <Grid container spacing={2} className='aboutHeader' alignContent='space-evenly' direction='row'>
+    <Grid root spacing={2} className='aboutHeader' alignContent='space-evenly' noWrap='true'>
+    <Grid container>
     <Grid item xs={12} s={12} md={10} lg={4} spacing={0} alignItem='center'>
     <img
       className="profilePic"
       alter='Natalie Kendrick' 
       src={profile}
-      >
-     </img> 
+     />
     </Grid>
-     <Grid item xs={12} s={12} md={10} lg={8} spacing={6}>
-       <Grid item spacing={2}  className="aboutName" >Natalie Kendrick</Grid>
-       <Grid item spacing={4} className="aboutQuote">   
-    <em>
-    “The most dangerous phrase in the language is we've always done”
-    </em> ~ Grace Hopper</Grid>
+      <Grid div item xs={12} s={12} md={10} lg={4} spacing={0} alignItem='center'>
+
+     <p className='aboutName'>Natalie Kendrick</p>
+      
+     <div className='aboutQuote'> 
+      <em>
+      “The most dangerous phrase in the language is we've always done”
+    </em> ~ Grace Hopper
+    </div>
+      </Grid>
      </Grid> 
 
-
-    <Grid item className="aboutBody" spacing={2}>
+    <Grid container>
     <Typography paragraph>
     I am a motivated professional with extensive experiences ranging from web development to fine arts to customer service. I bring a unique protective to the table. I believe in continuous learner and big picture thinking. I am flexible, organized and determined.
     </Typography>
