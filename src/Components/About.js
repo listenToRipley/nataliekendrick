8bfdd56c4f35872627267e1922f275e838ba7c44
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2021-01-25 14:29:29 -0600

    adjust the profile size for medium

--- src/Components/About.js
+++ src/Components/About.js
@@ -10,7 +10,7 @@ const About = () => {
     <Paper className="aboutPage">
     <Grid root spacing={2} className='aboutHeader' alignContent='space-evenly' noWrap='true'>
     <Grid container>
-    <Grid item xs={12} s={6} md={6} lg={3} spacing={2} alignItem='center'>
+    <Grid item xs={12} s={6} md={5} lg={3} spacing={2} alignItem='center'>
     <img
       className="profilePic"
       alt='Natalie Kendrick' 
@@ -22,6 +22,9 @@ const About = () => {
      <p 
     className='aboutName'
     >Natalie Kendrick</p>
+    <div className='pronouns'>
+      <em>pronouns: she/her/hers</em>
+    </div>
       
      <div className='aboutQuote'> 
       <em>
@@ -31,9 +34,9 @@ const About = () => {
       </Grid>
 
     <Typography paragraph>
-    I am a motivated professional with extensive experience ranging from web development and fine arts to customer service. I bring a unique perspective to the table. I believe in continuous learning and big picture thinking. I am flexible, organized, goal-oriented, and creatively driven. 
+    Creatively drive, flexible, organize, goal oriented and bringing a unique perspective to the table. Natalie is a motivated professional with extensive experience ranging from web development and fine arts to customer service. 
     </Typography>
-    <Typography> I am passionate about making sure everyone gets what they need from the project. Being team oriented means to me that I keep an eye on my team to make sure they have what they need to succeed to the best of their ability. I believe that my success benefits the team above all else. When it comes to problem solving I am proactive, a self-starter, and enjoy problem solving.
+    <Typography> Natalie is passionate about making sure everyone gets what they need from the project. Being team oriented means that she keeps an eye on her team to make sure they have what they need to succeed to the best of their ability. I believe that my success benefits the team above all else. When it comes to problem solving I am proactive, a self-starter, and enjoy problem solving.
 </Typography>
     <Typography paragraph>
     In my spare time I love to read, hike with my dog, do yoga, and play board games. I love, love, love food and cooking. I am also a massive movie buff and enjoy international television. I look forward to hearing from you and I hope you have a fantastic adventure today. 
