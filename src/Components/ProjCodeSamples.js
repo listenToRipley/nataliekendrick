6747d8af5aa8cee948d09cf2baedb2ace48b75a0
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2021-01-31 22:36:47 -0600

    updates made to sample code

--- src/Components/ProjCodeSamples.js
+++ src/Components/ProjCodeSamples.js
@@ -16,18 +16,34 @@
 
 //https://github.com/AustinCodingAcademy/311_wk6_day2_authentication/blob/master/index.js
 import React from 'react'
+import {makeStyles} from '@material-ui/core';
 import Typography from '@material-ui/core/Typography';
 import SampleBank from './SampleBank';
-import SampleMastermind from './SampleMastermind'
+import SampleMastermind from './SampleMastermind';
+
+const useStyles = makeStyles({ 
+  sample: {
+    display: 'flex',
+    flexDirection: 'column',
+    width: '75%',
+    marginLeft: '12%'
+  },
+  profiles: {
+    marginTop: '2em',
+    color: 'white'
+  }
+})
 
 const CodeSamples = () => {
 
+  const classes = useStyles();
+
   return (
-    <div className='codeSamples'>
+    <div className={classes.sample}>
       <Typography variant='h5'>Sample code : </Typography>
       <SampleBank/>
       <SampleMastermind/>
-     <div className='sampleProfiles'>
+     <div className={classes.profiles}>
      <Typography paragraph> If you would like to review additional project,  please feel free to go to <a href="https://repl.it/@listenToRipley">repl.  it</a> or my <a href="https://www.codewars.com/users/minonOfLight">code   wars</a> profiles</Typography>
        <Typography paragraph>As well as my <a href="https://github.com/  listenToRipley">github page</a></Typography>
      </div>
