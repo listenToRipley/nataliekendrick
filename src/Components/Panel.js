ee6cf8906101b55950fb92f6e50e24fb37b507da
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2021-02-01 22:15:44 -0600

    decided again a different mobile panel and removed it

--- src/Components/Panel.js
+++ src/Components/Panel.js
@@ -1,17 +1,89 @@
-import React from 'react';
-import MobilePanel from './MobilePanel';
-import WebPanel from './WebPanel';
+import React, {useState} from 'react';
+import { makeStyles } from '@material-ui/core';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import {Link} from 'react-router-dom';
+import './Components.css';
 
+const useStyles = makeStyles({ 
+  panel:{
+    display: 'flex',
+    flexWrap: 'wrap',
+    color: 'transparent',
+    position:'static',
+    height: '20%',
+    marginBottom: '12em'
+  },
+  bar:{
+    padding: '1em',
+    position: 'absolute'
+  },
+  tabs:{ 
+    width: '100%',
+    color: 'white',
+    justifyContent: 'space-evenly',
+  },  
+  link: {
+    fontSize: '2.75em',
+    padding: '.2em'
+  }
+});
 
-const Panel = () =>  {
- 
-  const width = window.innerWidth
+const WebPanel = () =>  {
 
-  const breakPoint = 650
+  const classes = useStyles();
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+    //will need to include routing on click, use links. 
+  };
 
   return (
-   width < breakPoint ? <MobilePanel/> : <WebPanel/> 
+    <div className={classes.panel}>
+    <AppBar 
+    className={classes.bar}
+    color='transparent' >
+      <Tabs 
+      className={classes.tabs}
+      variant="fullWidth"
+      value={value} 
+      onChange={handleChange} 
+      aria-label="menu items" 
+      indicatorColor='primary' >
+        <Tab 
+          label="Home"
+          className={classes.link}
+          component={Link}
+          to='/' 
+          aria-label='home'
+        />
+        <Tab 
+          label="About"
+          component={Link}
+          className={classes.link}
+          to='/about'  
+          aria-label='about'
+        />
+        <Tab 
+          label="Resume"  
+          component={Link}
+          className={classes.link}
+          to='/resume' 
+          aria-label='resume'
+        />
+        <Tab 
+          label="Projects" 
+          className={classes.link}
+          component={Link}
+          to='/projects' 
+          aria-label='projects'
+        />
+      </Tabs>
+    </AppBar>
+  </div>
   );
 }
 
-export default Panel
+export default WebPanel
