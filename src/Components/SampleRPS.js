513ac9f1b104b2399ae104b81d939489aaa47216
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2021-02-04 21:28:42 -0600

    updated rps content

--- src/Components/SampleRPS.js
+++ src/Components/SampleRPS.js
@@ -26,8 +26,55 @@ const SampleRPS = () => {
       <Typography>Possible solution for rock, paper, scissors. pseudo code included.</Typography>
       <CodeMirror
         value='
-        '
-        options={{
+        `use strict`; &
+  // brings in the assert module for unit testing &
+const assert = require(`assert`); &
+// brings in the readline module to access the command line &
+const readline = require(`readline`); &
+// use the readline module to print out to the command line &
+const rl = readline.createInterface({ &
+  input: process.stdin,   &
+  output: process.stdout &
+}); &
+
+// the function that will be called by the unit test below &
+const rockPaperScissors = (hand1, hand2) => { &
+  // Use the unit test to see what is expected &
+
+  let formattedHand1 = hand1.trim().toLowerCase() &
+  let formattedHand2 = hand2.trim().toLowerCase() &
+  
+  const rh1 = (formattedHand1 === `rock`) 
+  const ph1 = (formattedHand1 === `paper`) 
+  const sh1 = (formattedHand1 === `scissors`) &
+  
+  const rh2 = (formattedHand2 === `rock`) 
+  const ph2 = (formattedHand2 === `paper`) 
+  const sh2 = (formattedHand2 === `scissors`) &
+  
+  const tie = (rh1 && rh2) || (sh1 && sh2) || (ph1 && ph2) 
+  const winner1 = (rh1 && sh2) || (ph1 && rh2) || (sh1 && ph2) 
+  const winner2 = (ph1 && sh2) || (rh1 && ph2) || (sh1 && rh2)  &
+  
+  let wrong = (!(winner1) && !(winner2)) &
+  
+  if (tie) { &
+    return "It`s a tie!" &
+  } &
+  else if (winner1) { &
+    return `Hand one wins!` &
+  
+  } else if (winner2) { &
+    return `Hand two wins!` &
+  
+  }  &
+  else if (wrong) { &
+    return `This is "Rock, Paper, Scissors" not "Ninjas, Wizards, Vikings", try again.`  &
+  } &
+  
+  } &
+  ' 
+        options={{ 
         mode: 'javascript',
         theme: 'material',
         lineNumbers: true,
