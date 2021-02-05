60c51df50f3058307094da22add0c632a65dd18c
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2021-02-04 20:48:58 -0600

    outline for controller sample

--- src/Components/SampleController.js
+++ src/Components/SampleController.js
@@ -1,1 +1,26 @@
-on
+import React from 'react'
+import { makeStyles } from '@material-ui/core';
+import {Controlled as CodeMirror} from 'react-codemirror2'
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/neat.css';
+import 'codemirror/mode/javascript/javascript.js';
+import 'codemirror/theme/material.css';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({ 
+  body:{
+    color: 'white'
+  }
+})
+
+const SamplerController = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.body}>
+      
+    </div>
+  )
+}
+
+export default SampleController
