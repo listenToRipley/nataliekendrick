d18b9295f510a6a3a5be8870219a5551f149517b
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2021-02-04 20:44:02 -0600

    updated doc for samples

--- src/Components/ProjCodeSamples.js
+++ src/Components/ProjCodeSamples.js
@@ -1,20 +1,3 @@
-//js
-
-//
-
-//https://github.com/listenToRipley/javascript-workbook/tree/gh-pages/03week
-
-//https://github.com/listenToRipley/javascript-workbook/tree/gh-pages/01week
-
-//backend
-
-//https://github.com/listenToRipley/311_checkpoint_1
-//https://github.com/listenToRipley/311_wk4_day2_node_mysql
-//https://github.com/listenToRipley/311_wk5_both_api_hackathon
-
-//https://github.com/listenToRipley/311_wk4_day2_node_mysql/blob/master/controllers/users.js
-
-//https://github.com/AustinCodingAcademy/311_wk6_day2_authentication/blob/master/index.js
 import React from 'react'
 import {makeStyles} from '@material-ui/core';
 import Typography from '@material-ui/core/Typography';
