a0ab603da53918bcf7bd6d13c5d30c7f03a7d689
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2021-02-08 16:53:44 -0600

    working?

--- server/server.js
+++ server/server.js
@@ -4,7 +4,10 @@ const port = process.env.PORT || 8080;
 const app = express();
 
 app.use(express.static(path.join(__dirname, '..', 'build')));
-app.use(express.static(public));
+
+app.get('/ping', (req, res) => {
+  return res.send('pong')
+})
 
 app.get((req, res, next) => {
   res.sendFile(path.join(__dirname, '..', 'build', 'index.html'))
