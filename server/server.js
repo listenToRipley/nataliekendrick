9ec309ae6b555e015312d0c8aefbafc1d1525eb7
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2021-02-08 17:18:38 -0600

    make sure its is up to date for netlify

--- server/server.js
+++ server/server.js
@@ -5,11 +5,7 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, '..', 'build')));
 
-app.get('/ping', (req, res) => {
-  return res.send('pong')
-})
-
-app.get((req, res, next) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'build', 'index.html'))
 })
 
