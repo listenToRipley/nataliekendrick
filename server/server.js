c053da2374fc84c330445804eb0d4d5fec8d6dc1
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2021-02-08 16:51:46 -0600

    very server is up

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, '..', 'build')));
 app.use(express.static(public));
 
-app.use((req, res, next) => {
+app.get((req, res, next) => {
   res.sendFile(path.join(__dirname, '..', 'build', 'index.html'))
 })
 
