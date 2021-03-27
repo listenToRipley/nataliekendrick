52f4e3927a2c05afd499689a6b4dc9300f2de946
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2021-03-27 15:48:26 -0500

    outlined the name

--- src/Components/ResumeHead.js
+++ src/Components/ResumeHead.js
@@ -5,10 +5,50 @@ import Typography from '@material-ui/core/Typography';
 import Link from '@material-ui/core/Link';
 import backdrop from './projectImgs/backdrop.jpeg';
 
+const useStyles = makeStyles((theme) => ({
+  resume: {
+    flexGrow: 1,
+    flexWrap: "wrap",
+    direction: "row",
+    minWidth: "200px",
+    fontFamily: "Droid Sans",
+    marginTop: "-3em",
+    marginBottom: "5em"
+  },
+  paper: {
+    padding: theme.spacing(2),
+    marginLeft: "5em", 
+    marginLeft: "auto",
+    marginRight: "auto",
+    marginBottom: "5em",
+    maxWidth: 900
+  },
+  separates: {
+    display: "flex",
+    color: "white",
+    backgroundImage: `url(${backdrop})`,
+    backgroundRepeat: "no-repeat",
+    justifyContent: "space-around",
+    //this needs to be bolder
+  },
+})
+);
+
 const ResumeHead = () => {
 
+  const classes = useStyles();
+
   return(
-    <div>Natalie Kendrick Header</div>
+    <div className={classes.paper}>
+    <Typography className={classes.name}>Natalie Kendrick</Typography>
+    <p className={classes.title}>Full Stack Developer</p>
+    <Paper className={classes.separates}>
+
+      <p classesName={classes.contact}>natalie.m.kendrick@austin.utexas.edu</p>
+      <p>520-404-3721</p>
+
+    </Paper>
+  </div>
   )
 } 
 
