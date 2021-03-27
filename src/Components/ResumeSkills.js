db7db2b7ff8bc491fd36952c1fbe8f6c93fd6ed7
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2021-03-27 15:42:03 -0500

    skill list

--- src/Components/ResumeSkills.js
+++ src/Components/ResumeSkills.js
@@ -1,6 +1,38 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import List from '@material-ui/core/List';
+import Link from '@material-ui/core/Link';
+import Button from '@material-ui/core/Button';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar'
+import ListItemText from '@material-ui/core/ListItemText';
+
 
 const ResumeSkills = () => {
+
+  const skills = [
+    "JavaScript",
+    "Node.js",
+    "React",
+    "Redux",
+    "MySQL",
+    "Express.js",
+    "HTML5",
+    "CSS",
+    "Debugging",
+    "Heroku",
+    "RESTful API",
+    "Git/GitHub",
+    "Strategic Planning",
+    "Project Management",
+    "Time Management",
+    "Customer Service",
+    "Contact Assessment",
+    "Workflow Creation",
+    ]
 
   return(
     <div>Skills</div>
