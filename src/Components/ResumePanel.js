83f7cab2c0e751621b60d86913177e4eac315b3e
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2021-03-27 15:34:13 -0500

    panel

---
+++ src/Components/ResumePanel.js
@@ -0,0 +1,10 @@
+import React from 'react';
+//would like to add a second menu under the main on so the different sections of the resume can be linked to
+const ResumePanel = () => {
+
+  return(
+    <div>Panel</div>
+  )
+} 
+
+export default ResumePanel
