7d0c8d748e3520b7fc7c456a57cd207bae43d9b5
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2021-03-27 15:43:13 -0500

    what I need for header

--- src/Components/ResumeHead.js
+++ src/Components/ResumeHead.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
+import backdrop from './projectImgs/backdrop.jpeg';
 
 const ResumeHead = () => {
 
