0a47b1cec6a1486504c70f481df3a0079e7884ce
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2021-03-27 16:21:56 -0500

    css on mission okay for now

--- src/Components/ResumeMission.js
+++ src/Components/ResumeMission.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles((theme) => ({ 
+  mission: {
+    marginTop: '-4em',
+    marginBottom: '2em'
+  }
+}));
 
 const ResumeMission = () => {
 
+  const classes = useStyles();
+
   return (
-    <Paper>
+    <p className={classes.mission}>
     After working in a variety of fields I have developed a comprehensive knowledge base for effective business practices, including:  application development, database design and the importance of leveraging those skills in creative and innovative approaches to problems while synthesizing the business and client needs.
-    </Paper>
+    </p>
   )
 
 };
