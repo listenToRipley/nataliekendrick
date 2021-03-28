e339b254db408adfa5ffd83fcdc5960bbec88058
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2021-03-28 17:08:33 -0500

    history made into accordian

--- src/Components/ResumeExp.js
+++ src/Components/ResumeExp.js
@@ -3,7 +3,11 @@ import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
 import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import List from '@material-ui/core/List';
 import Link from '@material-ui/core/Link';
 import ListItem from '@material-ui/core/ListItem';
@@ -31,6 +35,10 @@ const useStyles = makeStyles((theme) => ({
   },
   duty: {
     fontSize: 10
+  },
+  studentHeader: {
+    fontSize: theme.typography.pxToRem(15),
+    fontWeight: theme.typography.fontWeightRegular,
   },
   studentHistory: {
     display: 'flex',
@@ -325,12 +333,26 @@ const ResumeExp = () => {
       {profHistory}
     <br/>
 
-    <p>The jobs listed below were part time employment while Natalie was completing her education</p>
-    <br/>
+    <Accordion>
+      <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+          >
+          <Typography className={classes.heading}>Complete Work History</Typography>
+      </AccordionSummary>
+      <AccordionDetails>
+      <p>The jobs listed below were part time employment while Natalie was completing her education</p>
+      </AccordionDetails>
+
+      <br/>
     
     <div className={classes.studentHistory}>
     {studentHistory}
     </div>
+    </Accordion>
+    <br/>
+    
     </div>
   )
 } 
