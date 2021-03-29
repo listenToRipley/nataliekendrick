3fe8a3c03038bdc69d9f17c428c5f079c14db90c
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2021-03-28 19:07:12 -0500

    remove unneed

--- src/Components/ResumeHead.js
+++ src/Components/ResumeHead.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles((theme) => ({
