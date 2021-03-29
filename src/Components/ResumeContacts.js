179d39f26fcf42d4d3f561797b2eb34c75ace461
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2021-03-28 19:27:41 -0500

    remove unneed from contacts

--- src/Components/ResumeContacts.js
+++ src/Components/ResumeContacts.js
@@ -1,18 +1,10 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
-import List from '@material-ui/core/List';
-import Link from '@material-ui/core/Link';
 import Button from '@material-ui/core/Button';
 import ListItem from '@material-ui/core/ListItem';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import Avatar from '@material-ui/core/Avatar'
-import ListItemText from '@material-ui/core/ListItemText';
 import { FaGoogle } from "@react-icons/all-files/fa/FaGoogle";
 import { FaPhoneAlt } from "@react-icons/all-files/fa/FaPhoneAlt";
-import { FaMapMarkerAlt } from "@react-icons/all-files/fa/FaMapMarkerAlt";
 import { FaGithubAlt } from "@react-icons/all-files/fa/FaGithubAlt";
 import { FaLinkedin } from "@react-icons/all-files/fa/FaLinkedin";
 import { FaTwitterSquare } from "@react-icons/all-files/fa/FaTwitterSquare";
@@ -57,7 +49,7 @@ const useStyles = makeStyles((theme) => ({
 
 const ResumeContact = () => {
   const classes = useStyles();
-
+  //may want to update color of links
   const colors = []; 
 
   const contactDetails = [
