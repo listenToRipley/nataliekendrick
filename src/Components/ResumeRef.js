c218e01ca0e7b38a585d79b3d0dee99484f46a0e
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2021-03-28 19:10:26 -0500

    ref updated

--- src/Components/ResumeRef.js
+++ src/Components/ResumeRef.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import ListItem from '@material-ui/core/ListItem';
+import backdrop from './projectImgs/backdrop.jpeg';
+
+const useStyles = makeStyles((theme) => ({
+  sectionTitle: {
+    fontSize: 20,
+    marginTop: '-1em'
+  },
+  separates: {
+    display: 'block',
+    color: 'white',
+    backgroundImage: `url(${backdrop})`,
+    backgroundRepeat: 'no-repeat'
+  },
+  ref: {
+    margin: '1em'
+  }
+}));
+
+
 
 const ResumeRef = () => {
-//would like to add testimonals 
+  const classes = useStyles();
+
+//would like to add testimonials 
+
   return(
-    <div>References</div>
+    <div>
+    <Paper className={classes.separates}>
+      <ListItem className={classes.sectionTitle}>References</ListItem>
+    </Paper>
+    <Typography className={classes.ref}>References available upon request as well as complete work history</Typography>
+    </div>
   )
 } 
 
