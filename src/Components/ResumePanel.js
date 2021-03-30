b08de4b47c5eead24446f448dcb107462972be1a
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2021-03-29 22:24:53 -0500

    update content

--- src/Components/ResumePanel.js
+++ src/Components/ResumePanel.js
@@ -3,9 +3,9 @@ import { makeStyles } from '@material-ui/core/styles';
 import BottomNavigation from '@material-ui/core/BottomNavigation';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
 import FolderIcon from '@material-ui/icons/Folder';
-import RestoreIcon from '@material-ui/icons/Restore';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import LocationOnIcon from '@material-ui/icons/LocationOn';
+import  { FaArrowUp } from '@react-icons/all-files/fa/FaArrowUp'
 //would like to add a second menu under the main on so the different sections of the resume can be linked to
 
 const useStyles = makeStyles({ 
@@ -16,6 +16,10 @@ const useStyles = makeStyles({
     bottom: 0,
     left: 0
   },
+  icons: {
+    fontSize: '20px',
+
+  }
 });
 
 const ResumePanel = () => {
@@ -30,12 +34,12 @@ const handleChange = (event, newValue) => {
   return (
     <div className={classes.bottom}>
       <BottomNavigation value={value} onChange={handleChange} className={classes.root}>
-      <BottomNavigationAction label="Top" value="top" icon={<RestoreIcon />} />
-      <BottomNavigationAction label="Experience" value="experiences" icon={<FavoriteIcon />} />
-      <BottomNavigationAction label="Education" value="education" icon={<LocationOnIcon />} />
-      <BottomNavigationAction label="Skills" value="skills" icon={<FolderIcon />} />
-      <BottomNavigationAction label="Contacts" value="contacts" icon={<FolderIcon />} />
-      <BottomNavigationAction label="References" value="references" icon={<FolderIcon />} />
+      <BottomNavigationAction className={classes.icons} label="Top" value="top" icon={<FaArrowUp/>} />
+      <BottomNavigationAction className={classes.icons} label="Experience" value="experiences" icon={<FavoriteIcon />} />
+      <BottomNavigationAction className={classes.icons} label="Education" value="education" icon={<LocationOnIcon />} />
+      <BottomNavigationAction className={classes.icons} label="Skills" value="skills" icon={<FolderIcon />} />
+      <BottomNavigationAction className={classes.icons} label="Contacts" value="contacts" icon={<FolderIcon />} />
+      <BottomNavigationAction className={classes.icons} label="References" value="references" icon={<FolderIcon />} />
     </BottomNavigation>
   </div>
   );
