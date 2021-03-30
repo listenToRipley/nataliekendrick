d5a65b00d5c71b18b17d5466c5e5219baa9a9306
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2021-03-29 22:29:12 -0500

    edu icon update

--- src/Components/ResumePanel.js
+++ src/Components/ResumePanel.js
@@ -7,6 +7,8 @@ import FavoriteIcon from '@material-ui/icons/Favorite';
 import LocationOnIcon from '@material-ui/icons/LocationOn';
 import  { FaArrowUp } from '@react-icons/all-files/fa/FaArrowUp'
 import { FaBriefcase } from '@react-icons/all-files/fa/FaBriefcase';
+import { FaSchool } from '@react-icons/all-files/fa/FaSchool'; 
+import { School } from '@material-ui/icons';
 //would like to add a second menu under the main on so the different sections of the resume can be linked to
 
 const useStyles = makeStyles({ 
@@ -37,7 +39,7 @@ const handleChange = (event, newValue) => {
       <BottomNavigation value={value} onChange={handleChange} className={classes.root}>
       <BottomNavigationAction className={classes.icons} label="Top" value="top" icon={<FaArrowUp/>} />
       <BottomNavigationAction className={classes.icons} label="Experience" value="experiences" icon={<FaBriefcase/>} />
-      <BottomNavigationAction className={classes.icons} label="Education" value="education" icon={<LocationOnIcon />} />
+      <BottomNavigationAction className={classes.icons} label="Education" value="education" icon={<FaSchool />} />
       <BottomNavigationAction className={classes.icons} label="Skills" value="skills" icon={<FolderIcon />} />
       <BottomNavigationAction className={classes.icons} label="Contacts" value="contacts" icon={<FolderIcon />} />
       <BottomNavigationAction className={classes.icons} label="References" value="references" icon={<FolderIcon />} />
