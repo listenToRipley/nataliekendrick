b7d989212793abd22b43b4bd602ccc9677235fff
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2022-04-18 04:38:38 -0500

    resume head

--- src/Components/ResumeHead.js
+++ src/Components/ResumeHead.js
@@ -1,45 +1,45 @@
 import React from 'react';
-import { makeStyles } from '@mui/material/styles';
+// import { makeStyles } from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
 
-const useStyles = makeStyles((theme) => ({
-  resume: {
-    flexGrow: 1,
-    flexWrap: "wrap",
-    direction: "row",
-    minWidth: "200px",
-    fontFamily: "Droid Sans",
-    marginTop: "-3em",
-    marginBottom: "5em"
-  },
-  paper: {
-    padding: theme.spacing(2),
-    marginLeft: "auto",
-    marginRight: "auto",
-    marginBottom: "5em",
-    maxWidth: 900
-  },
-  name: {
-    fontSize: 32,
-    fontFamily: 'Droid Sans'
-  },
-  title: {
-    fontSize: 20,
-    marginTop: '.5em',
-    marginBottom: '-2em'
-  }
+// const useStyles = makeStyles((theme) => ({
+//   resume: {
+//     flexGrow: 1,
+//     flexWrap: "wrap",
+//     direction: "row",
+//     minWidth: "200px",
+//     fontFamily: "Droid Sans",
+//     marginTop: "-3em",
+//     marginBottom: "5em"
+//   },
+//   paper: {
+//     padding: theme.spacing(2),
+//     marginLeft: "auto",
+//     marginRight: "auto",
+//     marginBottom: "5em",
+//     maxWidth: 900
+//   },
+//   name: {
+//     fontSize: 32,
+//     fontFamily: 'Droid Sans'
+//   },
+//   title: {
+//     fontSize: 20,
+//     marginTop: '.5em',
+//     marginBottom: '-2em'
+//   }
 
-})
-);
+// })
+// );
 
 const ResumeHead = () => {
 
-  const classes = useStyles();
+  // const classes = useStyles();
 
   return(
-    <div className={classes.paper}>
-    <Typography className={classes.name} align="center">Natalie Kendrick</Typography>
-    <p className={classes.title} align="center">Software Developer</p>
+    <div className="paper">
+    <Typography className="name" align="center">Natalie Kendrick</Typography>
+    <p className="title" align="center">Software Developer</p>
   </div>
   )
 } 
