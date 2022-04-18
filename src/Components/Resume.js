88bd6a15b4df66792789cb17f243484bc732e974
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2022-04-18 04:30:20 -0500

    updated resume

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { makeStyles } from '@mui/material/styles';
+// import { makeStyles } from '@mui/material/styles';
 import Paper from '@mui/material/Paper';
 
 import ResumeHead from './ResumeHead';
@@ -11,36 +11,36 @@ import ResumeContact from './ResumeContacts';
 import ResumeRef from './ResumeRef'; 
 import ResumePanel from './ResumePanel';
 
-const useStyles = makeStyles((theme) => ({
-  resume: {
-    flexGrow: 1,
-    flexWrap: 'wrap',
-    direction: 'row',
-    minWidth: '200px',
-    fontFamily: 'Droid Sans',
-    marginTop: '-3em',
-    marginBottom: '5em'
-  },
-  paper: {
-    padding: theme.spacing(2),
-    marginLeft: 'auto',
-    marginRight: 'auto',
-    marginBottom: '5em',
-    maxWidth: 900,
-  },
+// const useStyles = makeStyles((theme) => ({
+//   resume: {
+//     flexGrow: 1,
+//     flexWrap: 'wrap',
+//     direction: 'row',
+//     minWidth: '200px',
+//     fontFamily: 'Droid Sans',
+//     marginTop: '-3em',
+//     marginBottom: '5em'
+//   },
+//   paper: {
+//     padding: theme.spacing(2),
+//     marginLeft: 'auto',
+//     marginRight: 'auto',
+//     marginBottom: '5em',
+//     maxWidth: 900,
+//   },
 
-}));
+// }));
 
 
 
 const Resume = () => {
-  const classes = useStyles();
+  // const classes = useStyles();
 
   return (
 
 <div>
-    <div className={classes.resume}>
-      <Paper className={classes.paper}>
+    <div className="resume">
+      <Paper className="paper">
 
       <ResumeHead/>
       <ResumeMission/>
