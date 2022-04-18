c070d5faf98b4d71d409030b1d963b5bee5fede8
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2022-04-18 04:34:31 -0500

    edu

--- src/Components/ResumeEdu.js
+++ src/Components/ResumeEdu.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { makeStyles } from '@mui/material/styles';
+// import { makeStyles } from '@mui/material/styles';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 import List from '@mui/material/List';
@@ -11,83 +11,83 @@ import aca from './projectImgs/acaBadge.png';
 import pima from './projectImgs/pima.png';
 import backdrop from './projectImgs/backdrop.jpeg';
 
-const useStyles = makeStyles((theme) => ({
-  eduBody: {
-    marginBottom: '3em'
-  },
-  sectionTitle: {
-    fontSize: 20,
-    marginTop: '-1em'
-  },
-  separates: {
-    display: 'block',
-    color: 'white',
-    backgroundImage: `url(${backdrop})`,
-    backgroundRepeat: 'no-repeat',
-    marginBottom: '1em',
-  },
-  eduIcons: {
-    marginRight: '4em',
-    overflow: 'auto',
-  },
-  eduContact: {
-    underline: 'hover'
-  },
-  eduLocation: {
-   display: 'block',
-   textAlign: 'end',
-   marginTop: '-4em',
-  },
-  degree: {
-    display: 'block',
-    marginLeft:'1em',
-    marginRight: '1em',
-    marginBottom: '2em',
-    fontSize: '1em',
-    fontStyle: 'italic',
-    fontWeight:' fontWeightLight',
-  }
+// const useStyles = makeStyles((theme) => ({
+//   eduBody: {
+//     marginBottom: '3em'
+//   },
+//   sectionTitle: {
+//     fontSize: 20,
+//     marginTop: '-1em'
+//   },
+//   separates: {
+//     display: 'block',
+//     color: 'white',
+//     backgroundImage: `url(${backdrop})`,
+//     backgroundRepeat: 'no-repeat',
+//     marginBottom: '1em',
+//   },
+//   eduIcons: {
+//     marginRight: '4em',
+//     overflow: 'auto',
+//   },
+//   eduContact: {
+//     underline: 'hover'
+//   },
+//   eduLocation: {
+//    display: 'block',
+//    textAlign: 'end',
+//    marginTop: '-4em',
+//   },
+//   degree: {
+//     display: 'block',
+//     marginLeft:'1em',
+//     marginRight: '1em',
+//     marginBottom: '2em',
+//     fontSize: '1em',
+//     fontStyle: 'italic',
+//     fontWeight:' fontWeightLight',
+//   }
 
-}));
+// }));
 
 const ResumeEdu = () => {
 
-  const classes = useStyles();
+  // const classes = useStyles();
 
   return(
-    <div className={classes.eduBody}>
+    <div className="eduBody">
       <List>
-        <Paper className={classes.separates}>
-          <ListItem className={classes.sectionTitle}>Education</ListItem>
+        <Paper className="separates">
+          <ListItem className="sectionTitle">Education</ListItem>
         </Paper>
-          <ListItem className={classes.eduIcons}>
+          <ListItem className="eduIcons">
           <ListItemAvatar>
             <Avatar 
-            className={classes.avatar}
+            className="avatar"
             alter='AustinCodingAcademy Badge' 
             src={aca}
             />
           </ListItemAvatar>
-          <Link className={classes.eduContact}
+          <Link className="eduContact"
            href='https://austincodingacademy.com/'>AUSTIN CODING ACADEMY</Link>
           </ListItem>
-          <Typography className={classes.degree}>Full Stack Developer  (Dec 2020)</Typography>
-          <ListItem className={classes.eduLocation}>Austin, TX</ListItem>
+          <Typography className="degree">Full Stack Developer  (Dec 2020)</Typography>
+          <ListItem className="eduLocation">Austin, TX</ListItem>
           <ListItem
-          className={classes.eduIcons}>
+          className="eduIcons">
           <ListItemAvatar>
             <Avatar 
-            className={classes.avatar}
+            className="avatar"
             alter='Pima Community College Logo' 
             src={pima}
             />
           </ListItemAvatar>
           <Link
-          className={classes.eduContact}
+          className="eduContact"
           href='https://www.pima.edu/'> PIMA COMMUNITY COLLEGE </Link>
           </ListItem>
-          <Typography className={classes.degree}>General Studies Associate's Degree Liberal Arts (Dec 2010)</Typography>
-          <ListItem className={classes.eduLocation}>Tucson, AZ</ListItem>
+          <Typography className="degree">General Studies Associate's Degree Liberal Arts (Dec 2010)</Typography>
+          <ListItem className="eduLocation">Tucson, AZ</ListItem>
             </List>
     </div>
   )
