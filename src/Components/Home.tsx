c68a911524b0ded99f9c8cba852511396345cf98
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2022-10-17 21:15:21 -0400

    first compontent

---
+++ src/Components/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react'; 
+
+function Home(): JSX.Element {
+  return <div>
+    <h1>Natalie Kendrick</h1>
+    <h3>Software Developer</h3>
+  </div>
+  
+};
+
+export default Home; 
