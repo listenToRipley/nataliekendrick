34d39da9479e28ff9af81c66303b92108352a4a5
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2022-10-20 22:23:33 -0400

    reset up a quote call

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -1,23 +1,39 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import quotes from "../info/history/quotes.json";
 
 interface Quote {
   id: number;
   quote: string;
   author: string;
-  source:{
+  source?:{
     type: string;
     title: string;
   },
   bio: string; 
 };
 
-interface Intro {
-  name: string;
-  quote: Quote; 
-  pronouns: string; 
-};
+// interface Intro {
+//   name: string;
+//   quote: Quote; 
+//   pronouns: string; 
+// };
 
 const About = (): JSX.Element => {
+  const [quote, setQuote] = useState<Quote | undefined>(undefined) 
+
+  const findQuote = () => {
+    const quotesLen = Object.keys(quotes).length
+    let quoteId: number = Math.floor(Math.random() * quotesLen);
+    return quoteId
+  };
+
+  useEffect(() => {
+    const id: number = findQuote()
+    const newQuote: Quote = quotes[id]
+    return setQuote(newQuote)
+  }, [])
+  
+  console.log(quote)
 
   return (
     <div>
--- src/Routing.tsx
+++ src/Routing.tsx
@@ -1,11 +1,13 @@
 import React from 'react'; 
 import { Routes, Route } from 'react-router-dom';
 import Home from './Components/Home';
+import About from './Components/About';
 
 const Router = (): JSX.Element => {
   return ( 
     <Routes>
       <Route path="/" element={<Home/>}/>
+      <Route path="/about" element={<About/>}/>
     </Routes>
   )
 }; 
