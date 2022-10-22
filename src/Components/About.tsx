7eaf521c1c4a74b58b2c8c8b8d8e6dd6f7c64ee1
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2022-10-22 14:58:18 -0400

    about completed

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -56,6 +56,12 @@ const About = (): JSX.Element => {
         <em>pronouns: she/her/hers</em>
         {createQuote()}
       </div>
+      <div className="body">
+        <p>Creatively driven, flexible, organized, goal-oriented, and bringing a unique perspective to the table. Natalie is a motivated professional with extensive experience ranging from web development and fine arts to customer service.</p>
+        <p> When it comes to problem-solving, she is proactive and a self-starter who believes that amicable and efficient solutions can be found. A team player who knows her successes benefits the team as much as they do her.</p>
+        <p>She loves to read, hike with her dog, do yoga, and play board games in her spare time. In addition, she is passionate about food, cooking, and sharing those meals with great conversation. She is also a massive movie buff and enjoys international television.</p>
+        <p>She looks forward to hearing from you and hopes you have a fantastic adventure today.</p>
+      </div>
     </div>
   )
 };
