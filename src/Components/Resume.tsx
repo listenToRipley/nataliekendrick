cafcc2ff9bb6d16438cd1af5c8e3fdbfd931ad9a
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2022-11-01 19:38:26 -0400

    outline resume

---
+++ src/Components/Resume.tsx
@@ -0,0 +1,16 @@
+import React, {useEffect, useState} from 'react'; 
+
+const Resume = () => {
+
+  <div>
+    <div>Head</div>
+    <div>Mission</div>
+    <div>Experience</div>
+    <div>Skills</div>
+    <div>Contacts</div>
+    <div>References</div>
+  </div>
+
+}; 
+
+export default Resume;
