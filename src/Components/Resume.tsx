653613898df421937d692a4da3ee9a4cf419e19b
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2022-11-01 19:44:11 -0400

    class updates on resume

--- src/Components/Resume.tsx
+++ src/Components/Resume.tsx
@@ -3,11 +3,11 @@ import React, {useEffect, useState} from 'react';
 const Resume = () => {
 
   <div>
-    <div>
+    <div className='header'>
       <h1>Natalie Kendrick</h1>
       <p className='title'>Software Developer</p>
     </div>
-    <div>
+    <div className='mission'>
       <h2>Mission</h2>
       <p>After working in various fields, I have developed a comprehensive knowledge of business practices. This means I understand the importance of leveraging these skills to provide sustainable and meaningful practices that create innovative, efficient, and effective products to support all parties. </p>
     </div>
