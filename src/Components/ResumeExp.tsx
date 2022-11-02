10fc336358213bdd0f232e27e679a3e4cfaf9765
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2022-11-01 20:20:31 -0400

    added todos

--- src/Components/ResumeExp.tsx
+++ src/Components/ResumeExp.tsx
@@ -1,6 +1,12 @@
 import React from 'react'; 
 import workHistory from '../info/history/workHistory.json'
 
+//TODO: 
+// 1. Loop though the workHistory
+// 2. if the key includes professional, create an entry for each of those 
+// 3. if the key includes part time, create an entry for each of those 
+// 4. if the key includes volunteer, create an entry for those. 
+
 const ResumeExperience = () => {
 
   return(
