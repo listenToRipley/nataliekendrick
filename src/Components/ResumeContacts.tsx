0ff88d77e59dff2c6d68879afc3b8d9abc3cdff6
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2022-11-01 20:17:59 -0400

    resume contacts outlines

---
+++ src/Components/ResumeContacts.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import socialLinks from '../info/history/socialLinks.json';
+// TODO: 
+// 1. for each social link, create a link with icons
+
+const ResumeContacts = () => {
+  const phone: number = 5204043721;
+  const countryCode: number = 1; 
+
+
+  return (
+    <div>
+      <a href=''><p>Icon</p><p>title</p></a>
+      <p> {countryCode} + {phone}</p>
+    </div>
+  )
+};
+
+export default ResumeContacts
