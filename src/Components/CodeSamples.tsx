fae83880dc126be91c9b2b80a51d0ff558a414f5
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2023-01-14 21:17:35 -0500

    code samples

---
+++ src/Components/CodeSamples.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const CodeSamples = (): JSX.Element => { 
+
+
+  return (
+    <div>
+      <div>
+      <h3>TITLE</h3>
+      <p>Description</p>
+        <pre>
+          <code>
+            placeholder
+          </code>
+        </pre>
+      </div>
+    </div>
+  )
+};
+
+export default CodeSamples
