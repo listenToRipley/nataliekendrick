5ebade66ba8249f0aaabb72c4d83178376d3f7fc
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2023-01-24 17:06:19 -0500

    remove errors

--- src/Components/CodeSamples.tsx
+++ src/Components/CodeSamples.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import caesarCipherWKey from "../../src/Samples/caesar";
-import maxstockprofit from "../../src/Samples/maxStockProfit";
-import sieveoferatstense from "../../src/Samples/sieveOfEratstenes";
+import maxStockProfit from "../../src/Samples/maxStockProfit";
+import sieveOfEratosthenes from "../../src/Samples/sieveOfEratstenes";
 
 const CodeSamples = (): JSX.Element => { 
 
@@ -16,13 +16,13 @@ const CodeSamples = (): JSX.Element => {
       "title": "Max Stock Provide",
       "description": "Takes in an array of prices and returns the max possible profit for the day.  In other words, find the lowest point and the highest point. Subtract the value from the highest to the lowest and return that value. ",
       "language": "javascript",
-      "code": maxstockprofit
+      "code": maxStockProfit
     },
     {
       "title": "Sieve Of Eratosthenes",
       "description": "Find all the prime numbers up to a given number.",
       "language": "javascript",
-      "code": sieveoferatstense
+      "code": sieveOfEratosthenes
     }
   ];
 
