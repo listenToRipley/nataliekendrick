937d9de49bbe0982dda13ec31c7ddc140063571c
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2023-01-24 20:37:07 -0500

    template complete for projects. updated todo

--- src/Components/Projects.tsx
+++ src/Components/Projects.tsx
@@ -5,7 +5,7 @@ import projects from "../info/history/projects.json"
 const Projects = (): JSX.Element => {
 
   //TODO:
-  // 1. Loop each json project and provide components
+  // 1. Add images for projects
   interface Project {
     "id": number,
     "projectName": string,
@@ -28,6 +28,29 @@ const Projects = (): JSX.Element => {
 
   return (
     <div>
+      <div>
+        {projectsList.map((proj: any) => {
+          return (
+            <div key={proj.id}>
+              <p>{proj.projectName}</p>
+              <p>placeholder for image for now</p>
+              <p>Completed: <span>{proj.completed.year}</span></p>
+              <div>Summary: 
+                <span>{proj.description}</span>
+                <br />
+                <p>Skills:</p><ul>{proj.skills.map((skill:string) => {
+                  return (
+                    <li>{skill}</li>
+                  )
+                })}</ul>
+              </div>
+              <a href={proj.site}>View Work</a>
+              <br />
+              <a href={proj.code}>View Code</a>
+            </div>
+          )
+        })}
+      </div>
       <div>
         <CodeSamples/>
       </div>
