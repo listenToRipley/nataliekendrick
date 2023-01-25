2d8ec9da93186940b8c35dc761aa61cd8b719c42
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2023-01-24 20:29:54 -0500

    import projects complete with interface

--- src/Components/Projects.tsx
+++ src/Components/Projects.tsx
@@ -1,16 +1,36 @@
 import React from "react";
 import CodeSamples from "./CodeSamples";
+import projects from "../info/history/projects.json"
 
 const Projects = (): JSX.Element => {
 
   //TODO:
-  // 1. Import the projects JSON
-  // 2. Loop each and provide components
+  // 1. Loop each json project and provide components
+  interface Project {
+    "id": number,
+    "projectName": string,
+    "completed": {
+      "year": number,
+      "month": string
+    },
+    "description": string, 
+    "skills": string[],
+    "color": string,
+    "image": string,
+    "altText": string,
+    "site": string,
+    "code": string
+  }
 
+  const projectsList: Project[] = projects.projects;
+
+  console.log(projectsList)
 
   return (
     <div>
-      <CodeSamples/>
+      <div>
+        <CodeSamples/>
+      </div>
     </div>
   )
 
