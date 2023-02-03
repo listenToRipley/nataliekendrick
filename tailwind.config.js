1e68dd3fc401faf43b8ad0678cb17ffe3f699eda
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2023-02-02 20:30:36 -0500

    tailwind still not working

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   mode: 'jit',
   content: [
-    "./src/**/*.{html,tsx, js}",
-    "./Components/**/*.{html,tsx, js}"
+    "./src/**/*.{html,ts,tsx,js}",
+    "/src/Components/**/*.{html,ts,tsx,js}",
+    "./src/assets/**/*.{html,css}",
+    "./public/index.html"
   ],
   theme: {
     extend: {}
   },
+  plugins: []
   // purge: ['./src**/*.{js,jsx,ts,tsx}', './public/**/*.html'],
 };
