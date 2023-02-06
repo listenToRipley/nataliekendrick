3b566c94220ef37a4fe71df32d68f3592dee2b85
nken <nken@lle.rochester.edu>
2023-02-06 15:05:34 -0500

    work on mobile render

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -11,7 +11,7 @@ export default function Home() {
         key={index} 
         href={item.link} 
         hrefLang={item.title}
-        className="no-underline"
+        className="my-auto ml-auto block cursor-pointer pr-4"
         >
           <svg className="h-20 w-40 mx-auto" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d={item.icon.path.d} stroke="#000" strokeLinecap="round" strokeLinejoin="round"> 
@@ -28,13 +28,13 @@ export default function Home() {
   
 
   return (
-    <div className="justify-center ml-auto pl-6 pt-5 ">
+    <div className="min-w-[80] md:min-w-[40%] justify-center ml-auto pl-6 pt-5 ">
     <div id="homeName" className="text-yellow-500 justify-center">
       <div className="text-5xl">Natalie Kendrick</div>
       <div className="text-4xl">Software Developer</div>
     </div>
     
-    <div id="tab" className="w-full flex py-2 justify-center">
+    <div id="tab" className="w-full flex py-2 justify-center min-w-[80] md:min-w-[40%]">
       {links ? [...links] : <br/>}
     </div>
   </div>
