e4faa95e9115b0e88afee14f486b6a32d490d2b2
nken <nken@lle.rochester.edu>
2023-02-06 20:27:15 -0500

    projects looking better.

--- src/Components/Projects.tsx
+++ src/Components/Projects.tsx
@@ -6,6 +6,7 @@ const Projects = (): JSX.Element => {
 
   //TODO:
   // 1. Add images for projects
+  // 2. Add functionality for carousel
   interface Project {
     "id": number,
     "projectName": string,
@@ -27,39 +28,60 @@ const Projects = (): JSX.Element => {
   // console.log(projectsList)
 
   return (
-    <div className="bg-fixed h-screen min-h-screen object-cover p-8 scroll-smooth overflow-auto">
+    <div className="bg-fixed h-screen min-h-screen object-cover p-4 scroll-smooth overflow-auto ">
+
+      <div className="flex">
+
+          <div id="previous" className="text-5xl flex justify-center items-center cursor-pointer p-2 relative -top-8">
+            <p>"!"</p>
+          </div>
+          
+
+      <div id="carousel" className="mt-4 flex w-full flex-1 gap-4 overflow-x-auto scroll-smooth pb-5">
       <div className="flex">
         {projectsList.map((proj: any) => {
           const relative= "../assets/images/projectImgs/"+ proj.image
           console.log(relative)
           return (
-            <div key={proj.id} className="m-32 col-auto">
+            <div key={proj.id} className="min-w-[60] md:min-w-[15%] ml-4">
               <p>{proj.projectName}</p>
-              <div className="block h-20 w-20 bg-black/80">
-                <img src={relative} alt={proj.altText} />
+              <div className="h-80 w-full snap-center rounded-xl bg-cover bg-center shadow-md shadow-black/20">
+                <img className="h-20 w-20 rounded-full" src={relative} alt={proj.altText} />
               </div>
-              <div className="">
+              <div className="p-8 divide-y divide-slate-20">
               <p>Completed: <span>{proj.completed.year}</span></p>
-              <div className="flex"> 
-                <span>Summary:</span> 
+              <div className="flex flex-col"> 
+                <span className="italics">Summary:</span> 
                 <span className="bg-brown">{proj.description}</span>
                 <br />
               </div>
               <div>
-              <p>Skills:</p><ul className="flex">{proj.skills.map((skill:string) => {
+              <p>Skills:</p><ul className="">{proj.skills.map((skill:string) => {
                   return (
-                    <li>{skill}</li>
+                    <li className="first:pt-0 last:pb-0">{skill} </li>
                   )
                 })}</ul>
               </div>
+              <div>
               <a href={proj.site}>View Work</a>
-              <br />
+              </div>
+  
+              <div>
               <a href={proj.code}>View Code</a>
+              </div>
               </div>
             </div>
           )
         })}
       </div>
+      </div>
+
+      <div id="next" className="text-5xl flex justify-center items-center cursor-pointer p-2 relative -top-8">
+          <p>"!!"</p>
+          </div>
+
+      </div>
+      
       <div>
         <CodeSamples/>
       </div>
