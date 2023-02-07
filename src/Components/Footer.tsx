edc3443052a3731d49b8ce27dd7edc9225e5e25f
nken <nken@lle.rochester.edu>
2023-02-07 15:50:03 -0500

    formatting update

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
     return (
-        <div className="p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
+        <div className="p-4 bg-white rounded-md shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
             <div className="text-sm text-gray-500 sm:text-center dark:text-gray-400">* This site is in active development</div>
         </div>
     )
