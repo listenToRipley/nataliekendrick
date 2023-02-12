b835fd434aa50fdc6760adb099b116e953d5584e
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2023-02-11 21:10:07 -0500

    change about page display

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -49,29 +49,29 @@ const About = (): JSX.Element => {
     <div className="grid justify-center mx-10 my-10 auto-cols-auto md:flex md:flex-col ">
       <div className="flex flex-shrink mt-8">
         <img className="aspect-auto rounded-full max-h-80 group-open:rotate-45 transition-all relative" src={profile} alt="natalie kendrick"></img>
-        <div className=" text-sky-700 ml-6 mt-10 flex-col">
+        <div className="text-sky-700 ml-6 mt-24 md:flex-col">
           <div id="name" className="text-2xl mb-8 md:text-8xl">Natalie Kendrick</div>
-          <div id="pronouns" className="mt-4 text-xl text-sky-700 text-end italic">
+          <div id="pronouns" className="mt-4 text-xl text-end italic ">
             pronouns: she/her/hers
         </div>
         </div>
       </div>
 
-      <div className = "justify-center block">
-        <div id={`${quoteId}`} className="bg-white/20 transition-colors p-4 justify-center mt-10">
-          {createQuote()}
+      <div className="flex flex-wrap md:flex-nowrap mt-4 mb-5">
+        <div className="p-8">
+          <div id={`${quoteId}`} className="bg-black/40 text-teal-100 transition-colors p-6 mt-10">
+            {createQuote()}
+          </div>
         </div>
-  
-      </div>
-      <div className="bg-white/20 transition-colors justify-center mt-4">
-        <div className="p-4">
+
+        <div className="p-4 mt-8 bg-black/40 text-teal-100 transition-colors">
           <p className="pt-4 pb-4">Creatively driven, flexible, organized, goal-oriented, and bringing a unique perspective to the table. Natalie is a motivated professional with extensive experience ranging from web development and fine arts to customer service.</p>
           <p className="pb-4"> When it comes to problem-solving, she is proactive and a self-starter who believes that amicable and efficient solutions can be found. A team player who knows her successes benefits the team as much as they do her.</p>
           <p className="pb-4">She loves to read, hike with her dog, do yoga, and play board games in her spare time. In addition, she is passionate about food, cooking, and sharing those meals with great conversation. She is also a massive movie buff and enjoys international television.</p>
           <p className="pb-4">She looks forward to hearing from you and hopes you have a fantastic adventure today.</p>
         </div>
       </div>
-      <div className="flex justify-self-end self-auto md:self-end -mt-76">
+      <div className="flex justify-self-end self-auto md:self-end -mt-76 bg-black/40 text-teal-100">
         <ResumeContacts/>
         </div>
       </div>
