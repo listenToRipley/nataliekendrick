5968bd254b9c30e44f8ac3e8cdf8d628dd8ac033
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2023-02-11 22:23:09 -0500

    change order for links

--- src/Components/SocialLinks.tsx
+++ src/Components/SocialLinks.tsx
@@ -19,7 +19,7 @@ const SocialLinks = (): JSX.Element =>  {
         return (
             <div className="has-tooltip hover:stroke-teal-200 transition duration-200 ease-in-out m-2">
                 <span 
-                    className="tooltip shadow-lg p-1 mt-6 ml-20 duration-150 bg-gray-500 rounded-lg text-gray-900 ease-in-out">
+                    className="tooltip shadow-lg p-1 mt-6 ml-20 duration-150 bg-sky-700 rounded-lg text-teal-200 ease-in-out">
                     {item.title}
                 </span>
                 <a 
@@ -29,7 +29,7 @@ const SocialLinks = (): JSX.Element =>  {
                     target="_blank"
                     rel="noopener noreferrer"
                 >
-                <svg className="h-20 stroke-sky-600 drop-shadow-2xl bg-cyan-500 shadow-sky-800 aspect-auto rounded-full hover:stroke-teal-200 transition duration-150 ease-in-out " viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <svg className="h-20 hover:stroke-sky-500 drop-shadow-2xl hover:bg-sky-900 shadow-cyan-200 aspect-auto rounded-full stroke-teal-200 bg-sky-700 transition duration-150 ease-in-out " viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path 
                         d={item.icon.path.d} 
                         strokeLinecap="round" 
