d80192083d604545c39c5232ecdc44d5debfd978
listenToRipley <natalie.m.kendrick@gmail.com>
2023-09-11 10:43:41 -0400

    direct with Navigate

--- src/Routing.tsx
+++ src/Routing.tsx
@@ -1,11 +1,10 @@
 import React from "react"; 
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route,Navigate } from "react-router-dom";
 import Home from "./Components/Home";
 import About from "./Components/About";
 import Resume from "./Components/Resume";
 import Project from "./Components/Project";
 import RoutingError from "./Components/404";
-import { Redirect } from "react-router-dom";
 
 const Router = (): JSX.Element => {
 
@@ -18,7 +17,7 @@ const Router = (): JSX.Element => {
       <Route path="/resume" element={<Resume/>}/>
       <Route path="/projects" element={<Project/>}/>
       <Route path="/404" element={<RoutingError/>}/>
-      <Redirect to="/404"/>
+      <Route path="/*" element={<Navigate replace to="/404"/>}/>
     </Routes>
   )
 }; 
