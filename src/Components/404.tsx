20236a176aee78585a44d2e754e83bed2b5d3193
listenToRipley <natalie.m.kendrick@gmail.com>
2023-09-11 10:14:31 -0400

    outline 404 page

--- src/Components/404.tsx
+++ src/Components/404.tsx
@@ -2,7 +2,13 @@ import React from 'react';
 
 const RoutingError = () : JSX.Element => {
     return (
-        <div></div>
+        <div>
+            <button>Back</button>
+            <h1>Sorry</h1>
+            <img src="https://i.giphy.com/media/jOpLbiGmHR9S0/giphy.webp" alt="michael scott from the office, cringing" />
+            <p>This page was not found.</p>
+            <button>Return Home</button>
+        </div>
     );
 };
 
