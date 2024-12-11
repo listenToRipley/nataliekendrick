f9b8cdabff214c4eeaf436dcd8085e4c65d431b9
listenToRipley <natalie.m.kendrick@gmail.com>
2024-12-11 12:12:59 -0500

    formatting

--- src/Components/ResumeSkills.tsx
+++ src/Components/ResumeSkills.tsx
@@ -1,48 +1,37 @@
 import React from "react";
 import skills from "../assets/history/skills.json";
 
-const ResumeSkills = (): JSX.Element => {
-  
-  const soft: string[] = skills.soft.sort((a,b) => {
-    if (a<b) {return -1}
-    if (a>b) {return 1}
-    return 0
-  });
-  const tech: string[] = skills.technical.sort((a,b) => {
-    if (a<b) {return -1}
-    if (a>b) {return 1}
-    return 0
-  });
+const ResumeSkills = (): JSX.Element => { 
+  // Sort each category alphabetically
+  const sortSkills = Object.entries(skills).reduce((acc,[key, value])=> {
+    acc[key] = value.sort((a:string, b:string)=> a.localeCompare(b));
+    return acc;
+  }, {} as Record<string, string[]>);
 
-  return (
-    <div className="bg-white/20 transition-colors p-4 mt-10 flex">
-      <h3 className="text-2xl mr-4 underline">Skills</h3>
-      <div>
-        <div className="text-xl italic">Soft</div>
-        <ul className="p-2">
-          {soft.map((skill: string, id: number): JSX.Element => {
-            return (
-              <li key={id}>{skill}</li>
-            )
-          })
-          }
-        </ul>
-      </div>
+return (
 
-      <div>
-        <div className="text-xl italic">Technical</div>
-        <ul className="p-2 grid">
-          {tech.map((skill: string, id:number): JSX.Element  => {
-            return (
-              <li className="px-2 col-span-2" key={id}>{skill}</li>
-            )
-          })
-          }
-        </ul>
-      </div>
-
+  <div className="bg-white/20 transition-colors p-4 mt-10 flex">
+    <h3 className="text-2xl mr-4 underline">Skills</h3>
+    <div>
+      {Object.entries(sortedSkills).map(([category, skillsList]): JSX.Element => (
+        <div>
+          <div className="text-xl italic">{category.replace(/_/g, " ")}</div>
+          <ul>
+            {skillsList.map((skill: string, id: number) => {
+              return(
+                <li>
+                  {skill}
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+      ))}
     </div>
-  )
+  </div>
+
+);
+
 };
 
 export default ResumeSkills
