f74cfe6b07b7b5e99b506db5b0a2e56edd03642e
listenToRipley <natalie.m.kendrick@gmail.com>
2024-12-12 12:08:32 -0500

    change type of return type on skills to see if this breaks

--- src/Components/ResumeSkills.tsx
+++ src/Components/ResumeSkills.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import skills from "../assets/history/skills.json";
 
-const ResumeSkills = (): JSX.Element => { 
+const ResumeSkills = (): React.FC => { 
   // Sort each category alphabetically
   const sortSkills = Object.entries(skills).reduce((acc,[key, value])=> {
     acc[key] = value.sort((a:string, b:string)=> a.localeCompare(b));
@@ -13,12 +13,12 @@ return (
   <div className="bg-white/20 transition-colors p-4 mt-10">
     <h3 className="text-xl sm:text-2xl underline mb-4">Skills</h3>
     <div>
-      {Object.entries(sortSkills).map(([category, skillsList]): JSX.Element => {
+      {Object.entries(sortSkills).map(([category, skillsList]): React.FC => {
         return(
         <div className="flex flex-col lg:flex-row lg:items-start lg:gap-4 mb-6">
           <div className="text-lg sm:text-xl italic lg:w-1/4">{category.replace(/_/g, " ")}</div>
           <ul className="grid grid-cols-1 gap-2 lg:w-3/4">
-            {skillsList.map((skill: string, id: number): JSX.Element => {
+            {skillsList.map((skill: string, id: number): React.FC => {
               return (
                 <li className="px-2">{skill}</li>
               );
